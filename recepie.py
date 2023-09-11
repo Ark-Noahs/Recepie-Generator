26937ad309445ce5d50e4eefc97580b3b45eb438
Noe Chairez <95702080+Ark-Noahs@users.noreply.github.com>
2023-09-10 21:10:57 -0500

    Add files via upload

--- recepie.py
+++ recepie.py
@@ -1,143 +1,348 @@
 
+'''
+the following code is a recepie generator that asks the user what type of food they are craving. 
+From there it prints out different options from that category. 
+there are set lines to printout based off of what option user enters
 
-#     UPDATED: 09/09   rewrote code so that its simpler to read
-#    -stopped at American menu (1) , option 3
+future goal: expand from this, want to add different categories such as grilling, 
+    baking and more. As well as add 10 different ethnic foods and offer 3 course meals as well as offer choices from breakfast, 
+    lunch and dinner.
+    check back at end of 2023 fall semester for updated code!
+'''
+
 
 
 
 #main menu
-menu = {}
-menu['1'] = "American"
-menu['2'] = "Tex-Mex"
-menu['3'] = "Italian"
-menu['4'] = "Asian"
-menu['5'] = "Surprise me"
+menu = {
+    '1' : "American",
+    '2' : "Tex-Mex",
+    '3' : "Italian",
+    '4' : "Asian",
+    '5' : "Surprise me"
+}
 
 # Define the American menu
-americanMenu = {}
-americanMenu['1'] = "CheeseBurgers with fries"
-americanMenu['2'] = "HotDogs with chili"
-americanMenu['3'] = "Steak with mashed potatoes"
-americanMenu['4'] = "Lemon Pepper Wings with waffle fries"
-americanMenu['5'] = "Chicken fried Steak"
-americanMenu['6'] = "Go Back"# Option to go back to the previous menu
+americanMenu = {
+    '1': "Cheeseburgers with fries",
+    '2': "Hot Dogs with Chili",
+    '3': "Steak with mashed Potatoes",
+    '4': "Lemon Pepper Wings with Waffle fries",
+    '5': "Chicken fried Steak",
+    '6': "Go Back"#Option to go back to the previous menu
+}
 
-while True:
+texMexMenu = {
+    '1': "Baja shrimp tacos",
+    '2': "Classic Fajita Beef burrito",
+    '3': "Ground Beef Nachos",
+    '4': "Spicy Pork Chimichanga",
+    '5': "Fajita Chicken plate w rice and beans ",
+    '6': "Go Back"
+}
+
+italianMenu = {
+    '1': "Pepperoni Pizza",
+    '2': "Spaghetti and Meatballs",
+    '3': "Lasagna",
+    '4': "Chicken Tortellini",
+    '5': "Chicken Alfredo",
+    '6': "Go Back"
+}
+
+
+asianMenu = {
+    '1': "California Roll",
+    '2': "Stir Fry",
+    '3': "Shrimp Fried rice",
+    '4': "Vegan steamed Dumplings",
+    '5': "Pho Ga Nuong",
+    '6': "Go Back"
+}
+
+
+supriseMenu = {
+    '1': "Cajun Chicken pasta",
+    '2': "Pumpkin Bread",
+    '3': "Japanese fluffy pancakes",
+    '4': "Mango Lassi",
+    '5': "Butter Chicken with rice",
+    '6': "Go Back"
+}
+
+
+
+# Function to handle American menu
+def handle_american_menu():
+    print("\nAmerican")
+    
+    while True:
+        americanOptions = list(americanMenu.keys())
+        americanOptions.sort()
+        
+        for entry in americanOptions:
+            print(entry, americanMenu[entry])
+        
+        americanSelection = input("\nPlease Select an option for American:")
+        
+        if americanSelection in americanMenu:  # Check if it's a valid option
+            if americanSelection == '6':
+                break  # Go back to the previous menu
+            print("\n")
+            file_name = 'file1.txt'
+            try:
+                with open(file_name, 'r') as file:
+                    lines = file.readlines()
+                
+
+                menu_actions = {
+                    '1': (0, 46),  # cheeseburger/fries
+                    '2': (49, 94),  # hotdog w chili
+                    '3': (98, 148), #steak w mashed potatoes
+                    '4': (150, 210),#lemon pepper wings w waffle fries
+                    '5': (212, 267),#chicken fried steak
+                }
+                
+                if americanSelection in menu_actions:
+                    lines_range = menu_actions[americanSelection]
+                    
+                    for line in lines[lines_range[0]:lines_range[1]]:
+                        print(line.strip())
+                else:
+                    print("Invalid selection.")
+            except FileNotFoundError:
+                print(f"File '{file_name}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            
+            handle_user_choice()  
+        else:
+            print("Invalid selection. Please try again.")
+
+# Function to handle Tex-Mex menu
+def handle_TexMex_menu():
+    print("\nTexMex")
+    
+    while True:
+        texMexOptions = list(texMexMenu.keys())
+        texMexOptions.sort()
+        
+        for entry in texMexOptions:
+            print(entry, texMexMenu[entry])
+            
+        texMexSelection = input("\nPlease select one of the following Tex Mex dishes:")
+        if texMexSelection in texMexMenu:
+            if texMexSelection == '6':
+                break
+            print("\n")
+            file_name = 'file1.txt'
+            try:
+                with open(file_name, 'r') as file:
+                    lines = file.readlines()
+
+                menu_actions = {
+                    '1': (271, 326),# baja tacos
+                    '2': (330, 391), # fajita beef burrito 
+                    '3': (395, 445), #beef nachos 
+                    '4': (450, 506),#spicy pork chimichanga
+                    '5': (509, 578),#Fajita Chicken plate w rice/beans
+                }    
+                if texMexSelection in menu_actions:
+                    lines_range = menu_actions[texMexSelection]
+                    
+                    # Print lines from txt file
+                    for line in lines[lines_range[0]:lines_range[1]]:
+                        print(line.strip())
+                else:
+                    print("Invalid selection.")
+            except FileNotFoundError:
+                print(f"File '{file_name}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            
+            handle_user_choice()  
+        else:
+            print("Invalid selection. Please try again.")
+
+
+
+# Function to handle Italian menu
+def handle_Italian_menu():
+    print("\nItalian")
+    
+    while True:
+        italianOptions = list(italianMenu.keys())
+        italianOptions.sort()
+        
+        for entry in italianOptions:
+            print(entry, italianMenu[entry])
+            
+        italianSelection = input("\nPlease select one of the following Italian dishes:")
+        if italianSelection in italianMenu:
+            if italianSelection == '6':
+                break
+            print("\n")
+            file_name = 'file1.txt'
+            try:
+                with open(file_name, 'r') as file:
+                    lines = file.readlines()
+
+                menu_actions = {
+                    '1': (583, 632),# Pepperoni pizza
+                    '2': (635, 697), #  spaghetti w/ meatballs
+                    '3': (701, 762), #lasagna
+                    '4': (766, 817),#chicken tortellini
+                    '5': (822, 878),#chicken alfredo
+                }    
+                if italianSelection in menu_actions:
+                    lines_range = menu_actions[italianSelection]
+
+                    for line in lines[lines_range[0]:lines_range[1]]:
+                        print(line.strip())
+                else:
+                    print("Invalid selection.")
+            except FileNotFoundError:
+                print(f"File '{file_name}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            
+            handle_user_choice()  
+        else:
+            print("Invalid selection. Please try again.")
+            
+            
+            
+# Function to handle Asian menu
+def handle_Asian_menu():
+    print("\nAsian")
+    
+    while True:
+        asianOptions = list(asianMenu.keys())
+        asianOptions.sort()
+        
+        for entry in asianOptions:
+            print(entry, asianMenu[entry])
+            
+        asianSelection = input("\nPlease select one of the following Asian dishes:")
+        if asianSelection in asianMenu:
+            if asianSelection == '6':
+                break
+            print("\n")
+            file_name = 'file1.txt'
+            try:
+                with open(file_name, 'r') as file:
+                    lines = file.readlines()
+
+                menu_actions = {
+                    '1': (882, 932),#california roll
+                    '2': (936, 990),#stir fry 
+                    '3': (994, 1037),#shrimp fried rice 
+                    '4': (1041, 1094),#vegan steam dumplings
+                    '5': (1098, 1160),#Pho Ga Nuong
+                }    
+                if asianSelection in menu_actions:
+                    lines_range = menu_actions[asianSelection]
+                    
+                    # Print lines from txt file
+                    for line in lines[lines_range[0]:lines_range[1]]:
+                        print(line.strip())
+                else:
+                    print("Invalid selection.")
+            except FileNotFoundError:
+                print(f"File '{file_name}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            
+            handle_user_choice()  
+        else:
+            print("Invalid selection. Please try again.")
+
+
+
+# Function to handle Suprise menu
+def handle_Suprise_menu():
+    print("\nSo your undecisive on what you wanna eat?\nnot to worry here are a few meals that i had my classroom vote on\n(I reccomened the Cajun Chicken Pasta if you like spicy foods)\n")
+    
+    while True:
+        supriseOptions = list(supriseMenu.keys())
+        supriseOptions.sort()
+        
+        for entry in supriseOptions:
+            print(entry, supriseMenu[entry])
+            
+        supriseSelection = input("\nPlease select one of the following dishes:")
+        if supriseSelection in supriseMenu:
+            if supriseSelection == '6':
+                break
+            print("\n")
+            file_name = 'file1.txt'
+            try:
+                with open(file_name, 'r') as file:
+                    lines = file.readlines()
+                # Define menu actions with ranges from users input
+                menu_actions = {
+                    '1': (1164, 1219), #cajun chicken pasta 
+                    '2': (1222, 1273), #pumpkin bread 
+                    '3': (1277, 1329), #japanese fluffy bread
+                    '4': (1333, 1365), #mango lassi
+                    '5': (1368, 1431), #butter chicken w rice
+                }    
+                if supriseSelection in menu_actions:
+                    lines_range = menu_actions[supriseSelection]
+                    
+                    # Print lines from txt file
+                    for line in lines[lines_range[0]:lines_range[1]]:
+                        print(line.strip())
+                else:
+                    print("Invalid selection.")
+            except FileNotFoundError:
+                print(f"File '{file_name}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            
+            handle_user_choice()  
+        else:
+            print("Invalid selection. Please try again.")
+
+
+
+def handle_user_choice():#funct:asks the user to quit or return to previous subMenu
+    while True:
+        return_or_end = input("\nDo you want to go back to the previous menu (menu) or end the program (end)? ")
+        if return_or_end == 'menu':
+            break
+        elif return_or_end == 'end':
+            exit()
+        else:
+            print("Invalid input. Please enter 'menu' or 'end'.")
+
+
+while True:                                                         #Main starts 
     options = list(menu.keys())
     options.sort()
     print("Welcome to this recepie generator select one of the following to get started(1-6)")
     for entry in options:
         print(entry, menu[entry])
     print("Type out 'quit' to end program")
-
     selection = input("what are you craving today? ")
 
-    if selection == '1':                                #American menu selected
-        print("\nAmerican")
-        # Display the American menu
-        while True:
-            americanOptions = list(americanMenu.keys())
-            americanOptions.sort()
+    if selection == '1':
+        handle_american_menu()#American menu selected
 
-            for entry in americanOptions:
-                print(entry, americanMenu[entry])
+    elif selection == '2':                                         
+        handle_TexMex_menu() #Tex Mex menu selected 
 
-            americanSelection = input("\nPlease Select an option for American:").lower()
+    elif selection == '3':                                          
+        handle_Italian_menu()#Italian menu selected
 
-            if americanSelection == '1':#cheeseburger/fries
-                print("\n")
-                file_name = 'file1.txt'
-                try:
-                    with open(file_name, 'r') as file:
-                        lines = file.readlines()
-                # Print lines from txt file w/o line numbers
-                        for line in lines[:46]:
-                            print(line.strip())
-                except FileNotFoundError:
-                    print(f"File '{file_name}' not found.")
-                except Exception as e:
-                    print(f"An error occurred: {e}")
+    elif selection == '4':                                           
+        handle_Asian_menu()#Asian menu selected
         
-            
-                while True:#option to fo to American menu or end the program
-                    return_or_end = input("\nDo you want to go back to the previous menu (menu) or end the program (end)? ")
-                    if return_or_end == 'menu':
-                        break
-                    elif return_or_end == 'end':
-                        exit()
-                    else:
-                        print("Invalid input. Please enter 'menu' or 'end'.")
-
-            elif americanSelection == '2':#hotdog w chili
-                print("\n")
-                file_name = 'file1.txt'
-                try:
-                    with open(file_name, 'r') as file:
-                        lines = file.readlines()
-                #prints lines 45-94        
-                        for line in lines[49:94]:
-                            print(line.strip())
-                except FileNotFoundError:
-                    print(f"file '{file_name}' not found.")
-                except Exception as e:
-                    print(f"An error has occured: {e}")
-                
-                while True:
-                    return_or_end = input("\nDo you want to go back to the previous menu (menu) or end the program (end)? ")
-                    if return_or_end == 'menu':
-                        break
-                    elif return_or_end == 'end':
-                        exit()
-                    else:
-                        print("Invalid input. Please enter 'menu' or 'end'.")                
-                
-            elif americanSelection == '3':#Steak w/ mashed potatoes
-                print("\n")
-                file_name = 'file1.txt'
-                try:
-                    with open(file_name, 'r') as file:
-                        lines = file.readlines()
-                        #lines 97-148
-                        for line in lines[97:148]:
-                            print(line.strip())
-                except FileNotFoundError:
-                    print(f"file '{file_name}' not found ")
-                except Exception as e:
-                    print(f"an error has occured: {e}")
-                #ADD CODE SO THAT IT ASKS TO SELECT ANOTHER OPTION OR QUIT PROGRAM
-                
-                
-                
-                
-                
-                
-                
-            elif americanSelection == '4':
-                print("Lemon Pepper Wings with waffle fries")
-                # Your code for making Lemon Pepper Wings with waffle fries goes here
-            elif americanSelection == '5':
-                print("Chicken fried Steak")
-                # Your code for making Chicken fried Steak goes here
-            elif americanSelection == '6':
-                break  # Go back to the previous menu
-            
-            else:
-                print("Unknown Option Selected in American Menu!Please select one of the following\n")
-
-#work on this code once you get the top part running how you like it        
-    elif selection == '2':                                          #Tex Mex menu selected 
-        print("\nTex-Mex")
-        # Your code for deleting a student goes here
-    elif selection == '3':                                          #Italian menu selected
-        print("\nItalian")
-        # Your code for finding a student goes here
-    elif selection == '4':                                          #Asian menu selected 
-        print("\nAsian")
-    elif selection == '5':                                          #suprise menu selected 
-        print("\nSurprise me")
+    elif selection == '5':                                         
+        handle_Suprise_menu()#suprise menu selected 
         
     elif selection == 'quit':#ends the program
         break
     else:
         print("Unknown Option Selected!")
-
+        
